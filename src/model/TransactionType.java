167e9e1b61caee4d76039bc1f36ee4f40a771741
7errari <rickyricky118@hotmail.com>
2025-04-08 10:41:04 -0300

    Create TransactionType.java

---
+++ src/model/TransactionType.java
@@ -0,0 +1,21 @@
+package model;
+
+public enum TransactionType {
+    INCOME("Receita"),
+    EXPENSE("Despesa");
+    
+    private final String description;
+    
+    TransactionType(String description) {
+        this.description = description;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    @Override
+    public String toString() {
+        return description;
+    }
+}
